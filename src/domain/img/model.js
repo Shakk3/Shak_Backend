04b7b65c8c8b39320ed3464692586e4031d95a52
나병현 <nbhyun0329@gmail.com>
2024-09-27 18:24:35 +0900

    🔄 refactor: replace img class with TypeORM EntitySchema for database integration

--- src/domain/img/model.js
+++ src/domain/img/model.js
@@ -1,14 +1,38 @@
+const { EntitySchema } = require('typeorm');
 const { v4: uuidv4 } = require('uuid');
 
-class img {
-    constructor(data, name, view, date, reference) {
-        this.img_id = uuidv4;
-        this.data = data;
-        this.name = name;
-        this.view = view;
-        this.date = date;
-        this.reference = reference;
-    }
-}
-
-module.exports = img;
+module.exports = new EntitySchema({
+    name: 'Img',
+    tableName: 'img_tbl',
+    columns: {
+        img_id: {
+            type: 'uuid',
+            primary: true,
+            generated: 'uuid', 
+            default: uuidv4
+        },
+        data: {
+            type: 'longblob',
+            nullable: false,
+        },
+        name: {
+            type: 'varchar',
+            length: 50,
+            nullable: false,
+        },
+        view: {
+            type: 'int',
+            nullable: false,
+        },
+        date: {
+            type: 'date',
+            length: 20,
+            nullable: false,
+        },
+        reference: {
+            type: 'varchar',
+            length: 100,
+            nullable: false,
+        },
+    },
+});
